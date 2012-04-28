7aa490ba759a8e63098972ed0e6b512899e52f89
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2012-04-28 12:18:02 +0530

    import rspec

--- spec/models/import_spec.rb
+++ spec/models/import_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Import do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  #describe "validation" do
+  #	it "should not be valid without a import_date" do
+  #		aImport = Import.create(:import_date => "")
+  #		aImport.should_not be_valid
+  #	end
+  #end
+  
+  #describe "Associations" do
+  #	it "has many variant_updates" do
+  #		Import.new.should respond_to(:variant_updates)
+  #	end
+  #end
 end
