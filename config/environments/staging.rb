6686516c5b7634a1e9d1f58835df634eb07f0d09
AE Wible <aewible@gmail.com>
2012-05-01 09:57:01 +0200

    staging server logging fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -32,7 +32,6 @@ Fieldday::Application.configure do
 
   # See everything in the log (default is :info)
   config.log_level = :debug
-  config.logger = Logger.new(STDOUT)
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
